cbef3662d511fed455f6a2d6709184236c5287d4
Stefan Ziegler <edi.gonzales@gmail.com>
2018-02-17 16:10:40 +0100

    ?

--- stomp/client/main.js
+++ stomp/client/main.js
@@ -18,7 +18,7 @@ var connect_callback = function(frame) {
 
 var error_callback = function(frame) {
     // called back after the client is connected and authenticated to the STOMP server
-    console.log("error. not connected");
+    console.log("error. not connected.");
 };
 
 client.connect("", "", connect_callback, error_callback);
